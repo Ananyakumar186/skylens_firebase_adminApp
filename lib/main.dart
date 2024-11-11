ddebe8d9190403a2cad07dc4a320380ab1b57393
ananyakumar-0247 <Ananya.Kumar@eton-solutions.com>
2024-11-11 20:18:48 +0530

    login and dashboard pages added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:slad_app/auth/auth_service.dart';
 import 'package:slad_app/firebase_options.dart';
+import 'package:slad_app/pages/dashboard_page.dart';
+import 'package:slad_app/pages/login_page.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      routerConfig: _router,
     );
   }
+
+  final _router = GoRouter(
+      redirect: (context, state) {
+        if (AuthService.currentUser == null) {
+          return LoginPage.routeName;
+        }
+        return null;
+      },
+      routes: [
+        GoRoute(
+          name: DashboardPage.routeName,
+          path: DashboardPage.routeName,
+          builder: (context, state) => const DashboardPage(),
+        ),
+        GoRoute(
+          name: LoginPage.routeName,
+          path: LoginPage.routeName,
+          builder: (context, state) => const LoginPage(),
+        )
+      ]);
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String routeName = '/login';
+
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String _errMsg = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: const EdgeInsets.all(24.0),
+            shrinkWrap: true,
+            children: [
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Login', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      prefixIcon: Icon(Icons.email),
+                      labelText: 'Email Address'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Provide a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      prefixIcon: Icon(Icons.password),
+                      labelText: 'Password (at least 6 characters)'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Provide a valid password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              ElevatedButton(onPressed: _authenticate, child: const Text('Sign in as Admin')),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_errMsg, style: const TextStyle(fontSize: 18,color: Colors.red),),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _authenticate() {
+  }
+}
